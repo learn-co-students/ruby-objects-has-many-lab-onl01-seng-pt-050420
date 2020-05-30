5d225935b34793dd7b0bf88a36d3edf5709a142e
kschemel2010 <kschemel2010@gmail.com>
2020-05-29 23:54:11 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  attr_accessor :name, :artist
+
+  def initialize (name)
+    @name = name 
+    @songs = []
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    self.add_song(Song.new(name))
+  end
+
+  def self.song_count
+    Song.all.select {|song| song.artist}.length
+  end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,26 @@
+
+class Author
+
+  attr_accessor :name, :post
+
+ def initialize(name)
+  @name = name
+end
+
+def posts 
+  Post.all.select {|post| post.author == self }
+end
+
+def add_post (post)
+  post.author = self
+end
+
+def add_post_by_title(name)
+self.add_post(Post.new(name))
+end
+
+def self.post_count
+Post.all.select {|post| post.author}.length
+end
+
+end
