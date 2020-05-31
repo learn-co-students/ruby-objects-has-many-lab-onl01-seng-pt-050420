35aa6f3ba50c290c46fb020fe92812b59c962fdd
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-05-31 11:33:25 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+    attr_accessor :name, :author, :title
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
