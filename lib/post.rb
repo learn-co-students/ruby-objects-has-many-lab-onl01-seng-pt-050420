9f7f881d5749c39c32f20a6dc88bc44d8d407812
Michael Thomas <michaelthomas0611@gmail.com>
2020-06-05 23:51:38 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Post
+  attr_accessor :title
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
