411ba39f7f4976311f4b8c9335c1e0385735ea4d
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-15 00:41:05 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,33 @@
+require 'pry'
+
 class Author
   attr_accessor :name
 
-  @@post_count = 0
+  @@all = []
 
   def initialize(name)
     @name = name
-    @posts = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
   def add_post(post)
-    self.post << post
     post.author = self
-    @@post_count +=1
   end
 
-  def add_post_by_name(name)
-    post = Post.new(name)
-    self.posts << post
+  def add_post_by_title(title)
+    post = Post.new(title)
     post.author = self
-    @@post_count +=1
   end
 
   def posts
-    @posts
+    Post.all.select {|post| post.author == self}
   end
 
   def self.post_count
-    @@post_count
+    Post.all.count
   end
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,23 +1,18 @@
-class Post 
-  attr_accessor :title, :author 
-  
+class Post
+  attr_accessor :title, :author
+
   @@all = []
 
-def initialize(name, author=nil)
-    @name = name
-    @author = author
+  def initialize(title)
+    @title = title
     @@all << self
-  end
-
-  def author_name
-    if self.author
-      self.author.name
-    else
-      nil
-    end
   end
 
   def self.all
     @@all
+  end
+
+  def author_name
+    author.name if author
   end
 end
